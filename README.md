41a248b656cd0916ec9cd5b7b70d987f34b960ad
gittyvarshney <pv66645@gmail.com>
2023-04-09 22:19:47 +0530

    First library commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# React Test Component Library
+
+## Learning how to create the npm library with react and typescript
+
+License
+----
+NA
---
+++ src/components/Button.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+export interface ButtonProps {
+    backgroundColor?: string,
+    color?: string
+    children?: React.ReactNode
+    style?: React.CSSProperties
+    onButtonClick?: (e: React.MouseEvent<HTMLElement>) => void
+}
+
+export const Button: React.FC<ButtonProps> = (props) : React.ReactElement => {
+
+    const { children, backgroundColor, color, style, onButtonClick } = props;
+    let _style: React.CSSProperties = style || {};
+
+    if(backgroundColor) _style.backgroundColor = backgroundColor;
+    if(color) _style.color = color;
+
+    return (
+        <button style={_style} onClick={onButtonClick}>{children}</button>
+    )
+}
+
+export const Counter: React.FC<{}> = (props) => {
+
+    const [count, setCount] = useState<number>(0);
+
+    return(
+        <>
+        <p> Counter: {count}</p>
+        <Button backgroundColor='green' onButtonClick={(e) => { setCount(prev => prev + 1) }}>
+            Increment
+        </Button >
+        <Button backgroundColor='orange' onButtonClick={(e) => { setCount(prev => prev - 1) }}>
+            Decrement
+        </Button>
+        </>
+    )
+}
